b3ec891f203c4a1f8d1005b3958d55952682cc6b
gausul9365 <khurrram9365@gmail.com>
2025-05-06 07:48:02 +0530

    delete at end

--- CircularLinkedlIist.c
+++ CircularLinkedlIist.c
@@ -7,134 +7,196 @@ struct Node
   struct Node *next;
 };
 
-// -----------------------------------------insertiona at position-----------------------------------------
-void insertAtPosition(struct Node **head_ref, int data, int pos)
+void insertAtBegin(struct Node **head_ref, int new_data)
 {
-  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-  newNode->data = data;
+  struct Node *new_Node = malloc(sizeof(struct Node));
+  new_Node->data = new_data;
 
   if (*head_ref == NULL)
   {
-    if (pos == 1)
-    {
-      newNode->next = newNode;
-      *head_ref = newNode;
-    }
-    else
-    {
-      printf("Invalid position for empty list!\n");
-      free(newNode);
-    }
+    new_Node->next = new_Node;
+    *head_ref = new_Node;
     return;
   }
-
-  
-  if (pos == 1)
+  else
   {
     struct Node *temp = *head_ref;
+
     while (temp->next != *head_ref)
+    {
       temp = temp->next;
+    }
 
-    newNode->next = *head_ref;
-    temp->next = newNode;
-    *head_ref = newNode;
+    new_Node->next = *head_ref;
+    temp->next = new_Node;
+    *head_ref = new_Node;
+  }
+}
+
+void insertAtEnd(struct Node **head_ref, int new_data)
+{
+  struct Node *new_Node = malloc(sizeof(struct Node));
+  new_Node->data = new_data;
+
+  if (*head_ref == NULL)
+  {
+    new_Node->next = new_Node;
+    *head_ref = new_Node;
     return;
   }
 
   struct Node *temp = *head_ref;
+
+  while (temp->next != *head_ref)
+  {
+    temp = temp->next;
+  }
+
+  temp->next = new_Node;
+  new_Node->next = *head_ref;
+}
+
+void insertAtPosition(struct Node **head_ref, int new_data, int position)
+{
+  struct Node *new_Node = malloc(sizeof(struct Node));
+  new_Node->data = new_data;
+
+  if (*head_ref == NULL && position == 1)
+  {
+
+    new_Node->next = new_Node;
+    *head_ref = new_Node;
+  }
+
   int count = 1;
-  while (count < pos - 1 && temp->next != *head_ref)
-  {
-    temp = temp->next;
-    count++;
-  }
-
-  if (count < pos - 1)
-  {
-    printf("Position out of bounds\n");
-    free(newNode);
-    return;
-  }
-
-  newNode->next = temp->next;
-  temp->next = newNode;
-}
-
-//--------------------Insert at End------------------------------------------------
-void insertAtEnd(struct Node **head_ref, int data)
-{
-  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-  newNode->data = data;
-
-  if (*head_ref == NULL)
-  {
-    newNode->next = newNode;
-    *head_ref = newNode;
-    return;
-  }
-
   struct Node *temp = *head_ref;
   while (temp->next != *head_ref)
-    temp = temp->next;
-
-  temp->next = newNode;
-  newNode->next = *head_ref;
-}
-//------------------------------------------------------insert at begining-----------------------
-void insertAtBeginning(struct Node **head_ref, int data)
-{
-  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-  newNode->data = data;
-
-  if (*head_ref == NULL)
   {
-    newNode->next = newNode;
-    *head_ref = newNode;
+    count++;
+    temp = temp->next;
+  }
+
+  if (position < 1 || position > count + 1)
+  {
+    printf("Invalid position\n");
     return;
   }
 
-  struct Node *temp = *head_ref;  
-  while (temp->next != *head_ref)
-    temp = temp->next;
+  if (position == 1)
+  {
+    insertAtBegin(head_ref, new_data);
+    return;
+  }
 
-  newNode->next = *head_ref;
-  temp->next = newNode;
-  *head_ref = newNode;
+  if (position == count + 1)
+  {
+    insertAtEnd(head_ref, new_data);
+    return;
+  }
+
+  struct Node *current = *head_ref;
+
+  for (int i = 1; i < position - 1; i++)
+  {
+    current = current->next;
+  }
+  new_Node->next = current->next;
+  current->next = new_Node;
 }
 
-void display(struct Node *head)
+void deleteAtBegin(struct Node **head_ref)
 {
-  if (head == NULL)
+  if (*head_ref == NULL)
   {
     printf("List is empty\n");
     return;
   }
 
+  struct Node *head = *head_ref;
+  if (head->next == *head_ref)
+  {
+    free(head);
+    *head_ref = NULL;
+    return;
+  }
+
+  struct Node *last = head;
+  while (last->next != *head_ref)
+  {
+    last = last->next;
+  }
+
+  last->next = head->next;
+
+  *head_ref = head->next;
+  free(head);
+}
+
+void deleteAtEnd(struct Node **head_ref)
+{
+  if (*head_ref == NULL)
+  {
+    printf("List is empty\n");
+  }
+
+  struct Node *head = *head_ref;
+  // none node there
+  if (head->next == head)
+  {
+    free(head);
+    *head_ref = NULL;
+  }
+
   struct Node *temp = head;
+  // secondLast
+  while (temp->next->next != head)
+  {
+    temp = temp->next;
+  }
+
+  struct Node *last = temp->next;
+  temp->next = head;
+  free(last);
+}
+
+void display(struct Node *head_ref)
+{
+
+  if (head_ref == NULL)
+    return;
+
+  struct Node *temp = head_ref;
   do
   {
-    printf("%d -> ", temp->data);
+    printf("%d->", temp->data);
     temp = temp->next;
-  } while (temp != head);
+  } while (temp != head_ref);
+  printf("NULL\n");
 }
 
 int main()
 {
   struct Node *head = NULL;
 
-  printf("\nInserting at beginning:\n");
-  insertAtBeginning(&head, 10);
+  printf("insertion at begining\n");
+  insertAtBegin(&head, 23);
+  insertAtBegin(&head, 32);
+  insertAtBegin(&head, 22);
   display(head);
 
-  printf("\nInsertion at End\n");
-  insertAtEnd(&head, 20);
-  insertAtEnd(&head, 30);
-  insertAtEnd(&head, 40);
+  printf("Insert at End\n");
+  insertAtEnd(&head, 51);
   display(head);
 
-  printf("\nInsertion at Position\n");
-  insertAtPosition(&head, 5, 1);
+  printf("Insert at Position\n");
+  insertAtPosition(&head, 12, 2);
   display(head);
 
-  return 0;
+  printf("Delete at Begin\n");
+  deleteAtBegin(&head);
+  display(head);
+
+  printf("Delete At End\n");
+  deleteAtEnd(&head);
+  display(head);
 }
